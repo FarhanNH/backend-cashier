0a0d4ec5ac56052f02bba932f6d369fb4e4c1ce8
farhan.huda <farhan.huda@pactindo.com>
2023-01-30 09:12:11 +0700

    add refreshToken service

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -105,4 +105,36 @@ const login = async (req, res) => {
   }
 };
 
-export { register, login };
+const refreshToken = async (req, res) => {
+  try {
+    if (!req.body.refreshToken) {
+      throw { code: 428, message: 'REFRESH_TOKEN_REQUIRED' };
+    }
+
+    const verify = await jsonwebtoken.verify(req.body.refreshToken, env.JWT_REFRESH_TOKEN_SECRET);
+    if (!verify) {
+      throw { code: 401, message: 'REFRESH_TOKEN_INVALID' };
+    }
+
+    const payload = { _id: verify._id, role: verify.role };
+    const accessToken = await generateAccessToken(payload);
+    const refreshToken = await generateRefreshToken(payload);
+
+    return res.status(200).json({
+      status: true,
+      message: 'REFRESH_TOKEN_SUCCESS',
+      accessToken,
+      refreshToken,
+    });
+  } catch (err) {
+    console.log(err);
+    err.code = err.code || 500;
+
+    return res.status(err.code).json({
+      status: false,
+      message: err.message,
+    });
+  }
+};
+
+export { register, login, refreshToken };
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { register, login } from '../controllers/AuthController.js';
+import { register, login, refreshToken } from '../controllers/AuthController.js';
 
 var router = express.Router();
 
 router.post('/register', register); // auth/register
 router.post('/login', login); // auth/login
+router.post('/refresh-token', refreshToken); // auth/refreshToken
 
 export default router;
